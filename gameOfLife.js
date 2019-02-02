1a0881fc180ba52c9b4c10b088bf78c91a85c2e4
Lucy MacGlashan <lucy@Lucys-MBP.lan>
2019-02-02 11:25:55 +0000

    Init

---
+++ gameOfLife.js
@@ -0,0 +1,138 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    drawGrid();
+    generateStart();
+
+    // updateBoard();§§§
+}, false);
+
+function updateTiles(updatedTileStates)
+{
+    for(var i=0; i<updatedTileStates.length; i++)
+    {
+        var colour = updatedTileStates[i]["alive"] ? "rgb(56, 56, 56)" : "rgb(255, 255, 255)"
+        document.getElementById(updatedTileStates[i]["xPosition"] + "." + updatedTileStates[i]["yPosition"]).style.backgroundColor = colour
+    }
+}
+
+function getNumberOfNeighbours(tile)
+{
+    var numberOfNeighbours = 0;
+
+    var x = parseInt(tile["xPosition"])
+    var y = parseInt(tile["yPosition"])
+
+    var topLeft         = x-1 + "." + y-1
+    var topMiddle       = x   + "." + y-1
+    var topRight        = x+1 + "." + y-1
+    var middleRight     = x+1 + "." + y
+    var bottomRight     = x+1 + "." + y+1
+    var bottomMiddle    = x   + "." + y+1
+    var bottomLeft      = x-1 + "." + y+1
+    var middleLeft      = x-1 + "." + y
+
+    neighbourIds = [topLeft, topMiddle, topRight, middleRight, bottomRight, bottomMiddle, bottomLeft, middleLeft]
+
+    for(var i=0; i<neighbourIds.length; i++)
+    {
+        if(document.getElementById(neighbourIds[i]) != null)
+        {
+            if(document.getElementById(neighbourIds[i]).style.backgroundColor == "rgb(56, 56, 56)")
+            {
+                numberOfNeighbours ++
+            }
+        }
+    }
+
+    return numberOfNeighbours
+}
+
+function updateTileStates(currentTileStates)
+{
+    var updatedTileStates = currentTileStates;
+
+    for(var i=0; i<updatedTileStates.length; i++)
+    {
+        var numberOfNeighbours = getNumberOfNeighbours(updatedTileStates[i])
+
+        if(!updatedTileStates[i].alive)
+        {
+            if(numberOfNeighbours == 3)
+            {
+                updatedTileStates[i]["alive"] = true
+            }
+        }
+        else
+        {
+            if(numberOfNeighbours != 2 && numberOfNeighbours != 3)
+            {
+                updatedTileStates[i]["alive"] = false
+            }
+        }
+    }
+
+    updateTiles(updatedTileStates);
+    
+}
+
+function storeCurrentState()
+{
+    var tiles = document.getElementsByClassName('gridTile');
+    var currentTileStates = []
+
+    for(var i=0; i<tiles.length; i++)
+    {
+        var tile = tiles[i];
+        var id = tiles[i].id;
+
+        currentTileStates.push({"alive": tile.style.backgroundColor == "rgb(56, 56, 56)" ? true : false, "xPosition": id.split(".")[0], "yPosition": id.split(".")[1]})
+    }
+
+    return currentTileStates
+}
+
+function updateBoard()
+{
+    var currentTileStates = storeCurrentState();
+    updateTileStates(currentTileStates);
+}
+
+function generateStart()
+{
+    var numberOfStartTiles = Math.floor((Math.random() * 50) + 10);
+    var coordinatesOfStartTiles = [];
+
+    for(var i=0; i<numberOfStartTiles; i++)
+    {
+        var randomX = Math.floor((Math.random() * 40));
+        var randomY = Math.floor((Math.random() * 40));
+        coordinatesOfStartTiles.push(randomX + "." + randomY);
+    }
+
+    for(var i=0; i<coordinatesOfStartTiles.length; i++)
+    {
+        document.getElementById(coordinatesOfStartTiles[i]).style.backgroundColor = "rgb(56, 56, 56)";
+    }
+}
+
+function drawGrid()
+{
+    var tileSize = 20;
+    var numberOfTiles = 800/tileSize;
+
+    for(var i=0; i<numberOfTiles; i++)
+    {
+        for(var j=0; j<numberOfTiles; j++)
+        {
+            document.getElementById("gameGrid").innerHTML += '<div class="gridTile" id="' + i + '.' + j + '"></div>'
+        }
+    }
+
+    var tiles = document.getElementsByClassName("gridTile")
+    for(var i=0; i<tiles.length; i++)
+    {
+        tiles[i].style.width = tileSize + "px";
+        tiles[i].style.height = tileSize + "px";
+    }
+    
+}
